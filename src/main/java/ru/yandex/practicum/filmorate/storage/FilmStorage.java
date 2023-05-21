e013cc7d7bfb2c7b4a2811d0170e1e25d7e81afa
AleksandrDorofeev23 <110825244+AleksandrDorofeev23@users.noreply.github.com>
2023-05-22 02:13:42 +0300

    1.0.3 Чекстайл

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.storage;
 
-import org.springframework.jdbc.support.rowset.SqlRowSet;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.Collection;
